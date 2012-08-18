881692cfd2b8630a5bb603ebfd1d2c66cb5e0267
Kevin Sooley <omg.kev@gmail.com>
2012-08-17 21:03:23 -0400

    finally quitting idl.

--- readradmc.py
+++ readradmc.py
@@ -31,10 +31,10 @@ def readimage(filename=None, imagefile=None ):
 	nf=int(lines[2])
 	sizepix_x,sizepix_y=lines[3].split()
 	wavelength=lines[4]
-	temp=np.zeros(100*100)
+	temp=np.zeros(int(nx)*int(ny))
 	for i in range(len(lines[6:-1])): 
 		temp[i]=lines[6+i]
-	image=temp.reshape(-1,100).transpose()
+	image=temp.reshape(-1,int(nx)).transpose()
 	xi=(np.arange(int(nx))-int(nx)/2+0.5)*float(sizepix_x)
 	yi=(np.arange(int(ny))-int(ny)/2+0.5)*float(sizepix_y)
 	# do a contour plot of the image
@@ -57,15 +57,18 @@ def readimage(filename=None, imagefile=None ):
 #---------------------------------------------------------------------------
 #               read and plot the spectrum
 #---------------------------------------------------------------------------
+
 def B_lambda(T,wavelength):
 	h=6.62606957e-27
-	c=29979245800
+	c=299792458
 	kb=1.3806488e-16
 	return (2.0*h*c**2/wavelength**5)*(1.0/(np.exp(h*c/(wavelength*kb*T))-1.0)) 
 
 def readspectrum(filename = None, imagefile = None):
 	if filename is None:
 		filename = 'spectrum.out'
+	if imagefile is None:
+		imagefile = 'spectrum.png'
 	f = open(filename)
 	lines = f.readlines()
 	nlam = int(lines[1])
@@ -73,11 +76,14 @@ def readspectrum(filename = None, imagefile = None):
 	flux = np.zeros(nlam)
 	for i in range(nlam):
 		wavelength[i],flux[i] = lines[i+3].split()
-	wave=np.logspace(-1,12,num=300)
-	plt.loglog(wavelength,flux,'r-')
-	#plt.loglog(wave,B_lambda(4000,wave*10**-4),'b-')
-	plt.show()
-
+	wave=np.logspace(-8,1,num=3000)
+	plt.xlim(0.3e-6,1e-6)
+	#plt.ylim(10e-3,2)
+	plt.plot(wavelength*10**-6,flux/flux[0],'r-')
+	plt.plot(wave,B_lambda(18000,wave)/B_lambda(18000,0.3e-6),'b-')
+	plt.xlabel(r'$\lambda (\mu m)$')
+	plt.ylabel(r'Flux')
+	plt.savefig(imagefile)
 #---------------------------------------------------------------------------
 # 	Let's get fancy and call radmc3d from inside python
 #---------------------------------------------------------------------------
@@ -89,4 +95,5 @@ if __name__ == "__main__":
 	#readimage('image_V.out', 'image_V.png')
 	#readimage('image_R.out', 'image_R.png')
 	#readimage('image_I.out', 'image_I.png')
-	readspectrum()
+	#readspectrum()
+	readimage()
